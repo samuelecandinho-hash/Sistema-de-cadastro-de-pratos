03a616344cb1c0d1be46da1a54e6ebb82b756765
samueleliascandinho <samuel_e_candinho@estudante.sesisenai.org.br>
2026-08-17 12:04:20 -0300

    up:include table para index sem include

--- index.php
+++ index.php
@@ -15,10 +15,46 @@ include("infra/db/connect.php");
   <?php
 include("public/components/usuario.php");
 ?>
-    <?php
-    
-    include("public/components/table.php")
+    <h4>Pratos Cadastrados</h4>
 
+<table border="1" cellpadding="3">
+
+    <tr>
+        <th>ID</th>
+        <th>nome</th>
+        <th>descrição</th>
+        <th>preço</th>
+        <th>categoria</th>
+        <th>nome do usuario</th>
+        <th id="BotaoExcluir">Excluir</th>
+        <th>Editar</th>
+    </tr>
+
+    <?php
+    include ("infra/db/connect.php");
+    $sqlTodosPratos = "SELECT * FROM pratos";
+
+    $resultadoTodosPratos = $conn->query($sqlTodosPratos);
+
+    while($linha = $resultadoTodosPratos->fetch_assoc()){
+
+        echo "  <tr>
+                    <td>". $linha['id_pratos'] . "</td>
+                    <td>". $linha['nome_pratos'] . "</td>
+                    <td>". $linha['descricao_pratos'] . "</td>
+                    <td>". $linha['preco_pratos'] . "</td>
+                    <td>". $linha['categoria_pratos'] . "</td>
+                    <td>". $linha['nome_users'] . "</td>
+                    <td> <a href='excluir.php?id=". $linha['id_pratos'] ."'> Excluir</a></td>
+                    <td> <a href='editar.php?id=". $linha['id_pratos'] ."'> Editar</a></td>
+                </tr>
+        ";
+    }
+    
     ?>
+
+    
+
+</table>
 </body>
 </html>
