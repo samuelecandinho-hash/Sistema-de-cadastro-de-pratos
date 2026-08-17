a9373bc2e658e9e51f7a87998a140146b9a6c6e8
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-17 10:01:04 -0300

    add - start of the form

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include("infra/connect.php");
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1> Cadastro de pratos </h1>
+        <form action="POST">
+            <h2> Cadastro de pessoas </h2>
+            <label for="Nome"> Nome do arrombado: </label>
+            <input type="text" name="Nome">
+        </form>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
