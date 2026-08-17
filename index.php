359b45dbd7e8f9990d861672d3b5b4b3e7bb83ad
samueleliascandinho <samuel_e_candinho@estudante.sesisenai.org.br>
2026-08-17 10:13:35 -0300

    add:table

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
+session_start();
 
-include("infra\db/connect.php");
+include("../infra/db/connect.php");
+
+    $sql = "INSERT INTO usuarios (usuario,senha) 
+    VALUES ('$novoUsuario','$novaSenha')";  
+
+    if($conn->query($sql) === TRUE){
+        echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
+    }else{
+        echo "<script> alert('Erro ao cadastrar')</script>";
+    }
+
+};
 
 ?>
 
@@ -8,25 +20,34 @@ include("infra\db/connect.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
-    <header>
-        <h1> Cadastro de pratos </h1>
-        <form action="POST">
-            <h2> Cadastro de pessoas </h2>
-            <label for="Nome"> Nome do arrombado: </label>
-            <input type="text" name="Nome"> <br>
-            <label for="Email"> E-mail vagabundo: </label>
-            <input type="email" name="Email"> <br>
-            <button type="submit"> Botão que explode a lua </button>
-        </form>
-    </header>
-    <main>
+    <h3>Bem-Vindo!</h3>
+    <hr>
+    <h4>Cadastro de Novo Usuário.</h4>
+    <form method="POST">
+        <label>Usuário:</label>
+        <input type="text" name="usuario">
+        <br>
+        <label>Senha:</label>
+        <input type="password" name="senha">
+        <br>
+        <?php
+        
+            if(isset($erro)){
+                echo $erro;
+            };
+        
+        ?>
+        <br>
+        <button type="submit">Cadastrar</button>
+    </form>
+    <hr>
+    <?php
+    
+    include("components/table.php")
 
-    </main>
-    <footer>
-
-    </footer>
+    ?>
 </body>
 </html>
