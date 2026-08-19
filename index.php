8cdf3f79bc3bc31c9acf19d605c92b2ed6f3c9d8
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-19 12:45:45 -0300

    add - beaultiful css

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include("infra/db/connect.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
