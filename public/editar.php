9fa5426a9ede503072cb4b512dd7b6d9cc8905fa
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-19 08:58:08 -0300

    fix - a little bit of edit fixing

--- public/editar.php
+++ public/editar.php
@@ -4,13 +4,16 @@ session_start();
 
 include("../infra/db/connect.php");
 
+$id = $_GET['id'];
+
 $sql = "SELECT * FROM pratos WHERE id_pratos = $id";
 $resultado = $conn -> query($sql);
 $Pratos = $resultado -> fetch_assoc();
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $novoUsuario = $_POST["prato"];
-    $novaSenha = $_POST["senha"];
+    $novoNome = $_POST["prato"];
+    $novaDescricao = $_POST["senha"];
+    $novoPreco = 0;
 
     $sqlUpdate = " UPDATE usuarios SET usuario = '$novoUsuario', senha = '$novaSenha' WHERE id = $id";
 
@@ -33,16 +36,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 
 <h2>Editar Usuário</h2>
-<form method="POST">
-        <label>Usuário:</label>
-        <input type="text" name="usuario" value =" <?php echo $usuario['usuario'] ?>">
-        <br>
-        <label>Senha:</label>
-        <input type="password" name="senha" value =" <?php echo $usuario['senha'] ?>">
-        <br>
-        <br>
-        <button type="submit">Salvar</button>
-    </form>
-    
+<form method="POST" action="index.php">
+
+    <input type="hidden" name="formulario" value="prato">
+
+    <label for="nome_pratos">Nome:</label>
+    <input type="text" id="nome_pratos" name="nome_pratos" required>
+
+    <br>
+
+    <label for="descricao_pratos">Descrição:</label>
+    <input type="text" id="descricao_pratos" name="descricao_pratos" required>
+
+    <br>
+
+    <label for="preco_pratos">Preço:</label>
+    <input type="number" id="preco_pratos" name="preco_pratos" step="0.01" required>
+
+    <br>
+
+    <label for="categoria_pratos">Categoria:</label>
+    <input type="text" id="categoria_pratos" name="categoria_pratos" required>
+
+    <br>
+
+    <label for="nome_users">Nome do Usuário:</label>
+    <input type="text" id="nome_users" name="nome_users" required>
+
+    <br>
+
+    <button type="submit">Cadastrar Prato</button>
+
+</form>    
 </body>
 </html>
