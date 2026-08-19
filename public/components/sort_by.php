7f027ba773334f7130de3ef9c2a8e742afd86bf7
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-19 12:09:33 -0300

    add - trying the sort

--- public/components/sort_by.php
+++ public/components/sort_by.php
@@ -1,6 +1,16 @@
 <form method="POST" action="index.php">
     <h1> Procurar pratos de um usuário: </h1>
     <label for="nameSort"> Usuário: </label>
-    <input type="text" name="nameSort">
+    <select name="nameSort" id="123">
+        <?php 
+            $sqlUsuarios = "SELECT nome_users FROM users";
+            $resultadoUsuarios = $conn->query($sqlUsuarios);
+            echo "<select name=" . $nameSort . " id=" . $nameSort . ">";
+            while($linha = $resultadoUsuarios->fetch_assoc()){
+            echo   "<option value=" . $linha['nome_users'] . ">" . $linha['nome_users'] . "</option>";
+            }
+            echo "</select>"
+        ?>
+    </select>
     <button type="submit"> Procurar </button>
 </form>
