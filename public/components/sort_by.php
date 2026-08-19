3ddcb390c1200bebb392f18b3e74d15d8eb88909
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-19 12:15:23 -0300

    add - almost there!

--- public/components/sort_by.php
+++ public/components/sort_by.php
@@ -2,10 +2,10 @@
     <h1> Procurar pratos de um usuário: </h1>
     <label for="nameSort"> Usuário: </label>
     <select name="nameSort" id="123">
+        <option value = "Selecione"> Selecione </option>
         <?php 
             $sqlUsuarios = "SELECT nome_users FROM users";
             $resultadoUsuarios = $conn->query($sqlUsuarios);
-            echo "<select name=" . $nameSort . " id=" . $nameSort . ">";
             while($linha = $resultadoUsuarios->fetch_assoc()){
             echo   "<option value=" . $linha['nome_users'] . ">" . $linha['nome_users'] . "</option>";
             }
